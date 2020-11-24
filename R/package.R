4665fc7feae451532fb6492e943854da33f2989d
Yuri <yuri_ahuja@hms.harvard.edu>
2020-11-24 17:31:43 -0500

    Reorganized files into package

---
+++ R/package.R
@@ -0,0 +1,11 @@
+#' SAMGEP: A Semi-supervised Method for Prediction of Phenotype Event Times Using the Electronic Health Record
+#' 
+#' Semi-supervised Adaptive Markov Gaussian Embedding Process (SAMGEP) is a novel semi-supervised machine learning algorithm to predict phenotype event times using Electronic Health Record (EHR) data.
+#' 
+#' @docType package
+#' @name SAMGEP-package
+#' @keywords package
+#' @useDynLib SAMGEP
+#' @import stats mvtnorm nlme pROC abind nloptr
+#' @import foreach doParallel parallel
+NULL
